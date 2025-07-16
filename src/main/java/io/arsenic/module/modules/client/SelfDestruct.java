e71a36e1e97e5f4af1aab01eec2ddc52d88a0bbd
potatothelt5 <potatotheht5@gmail.com>
2025-07-16 21:36:43 +1000

    fixed it again

--- src/main/java/io/arsenic/module/modules/client/SelfDestruct.java
+++ src/main/java/io/arsenic/module/modules/client/SelfDestruct.java
@@ -1,35 +1,33 @@
 package io.arsenic.module.modules.client;
 
+import com.sun.jna.Memory;
+import io.arsenic.Arsenic;
 import io.arsenic.gui.ClickGui;
 import io.arsenic.module.Category;
 import io.arsenic.module.Module;
+import io.arsenic.module.setting.*;
 import io.arsenic.module.setting.BooleanSetting;
-import io.arsenic.module.setting.KeybindSetting;
 import io.arsenic.module.setting.Setting;
 import io.arsenic.module.setting.StringSetting;
-import io.arsenic.Arsenic;
-import io.arsenic.utils.EncryptedString;
 import io.arsenic.utils.Utils;
-import org.lwjgl.glfw.GLFW;
+
+import java.io.File;
 
 @SuppressWarnings("all")
 public final class SelfDestruct extends Module {
 	public static boolean destruct = false;
 
-	private final BooleanSetting replaceMod = new BooleanSetting(EncryptedString.of("Replace Mod"), true)
-			.setDescription(EncryptedString.of("Replaces the mod with the original JAR file"));
+	private final BooleanSetting replaceMod = new BooleanSetting("Replace Mod", true)
+			.setDescription("Repalces the mod with the original JAR file of the ImmediatelyFast mod");
 
-	private final BooleanSetting saveLastModified = new BooleanSetting(EncryptedString.of("Save Last Modified"), true)
-			.setDescription(EncryptedString.of("Saves the last modified date after self-destruct"));
+	private final BooleanSetting saveLastModified = new BooleanSetting("Save Last Modified", true)
+			.setDescription("Saves the last modified date after self destruct");
 
-	private final StringSetting downloadURL = new StringSetting(EncryptedString.of("Replace URL"),
-			"https://cdn.modrinth.com/data/5ZwdcRci/versions/FEOsWs1E/ImmediatelyFast-Fabric-1.2.11%2B1.20.4.jar");
+	private final StringSetting downloadURL = new StringSetting("Replace URL", "https://cdn.modrinth.com/data/5ZwdcRci/versions/FEOsWs1E/ImmediatelyFast-Fabric-1.2.11%2B1.20.4.jar");
 
 	public SelfDestruct() {
-		super(EncryptedString.of("Self Destruct"),
-				EncryptedString.of("Kills the system and destroys all traces of using this client. The client mod will be replaced as ImmediatelyFast Mod"),
-				-1,
-				Category.CLIENT);
+		super("SelfDestruct", "Kills the system and destroys all traces of using this client. The client mod will be replaced as ImmediatelyFast Mod", -1, Category.CLIENT);
+		setKey(-1);
 		addSettings(replaceMod, saveLastModified, downloadURL);
 	}
 
